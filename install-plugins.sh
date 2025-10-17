a7b729b95ea4d68ac604e087570fa345d78ae6c2
chubbyhippo <mkmsacc@outlook.com>
2025-10-17 23:32:05 +0700

    Add `com.joshestein.ideavim-quickscope` plugin to `install-plugins.sh`.

--- install-plugins.sh
+++ install-plugins.sh
@@ -10,6 +10,7 @@ $cmd installPlugins \
 	IdeaVIM \
 	com.github.camork.fileExpander \
 	com.jetbrains.kmm \
+  com.joshestein.ideavim-quickscope \
   com.julienphalip.ideavim.peekaboo \
 	com.joshestein.ideavim-quickscope \
 	eu.theblob42.idea.whichkey
